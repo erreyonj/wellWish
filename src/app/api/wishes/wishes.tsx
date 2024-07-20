ce594959d5fb5ced8a0a620424cd18eb26f57569
erreyonj <erreyon.js@gmail.com>
2024-07-20 16:59:01 -0500

    continue work on mongosetup

---
+++ src/app/api/wishes/wishes.tsx
@@ -0,0 +1,37 @@
+import Wish from "../../../components/models/Wish"
+import connectDB from "@/config/database";
+
+connectDB()
+
+
+module.exports = {
+    getProfile: async (req: NextRequest, res: NextResponse) => {
+        try {
+          const posts = await Post.find({ user: req.user.id });
+          res.render("profile.ejs", { posts: posts, user: req.user });
+        } catch (err) {
+          console.log(err);
+        }
+      },
+    createPost: async (req: NextRequest, res: NextResponse) => {
+        try {
+
+          console.log(req);
+          
+          // Upload image to cloudinary
+          const result = await cloudinary.uploader.upload(req.file.path);
+    
+          await Wish.create({
+            title: req.body.title,
+            img: result.secure_url,
+            cloudinaryId: result.public_id,
+            caption: req.body.caption,
+            user: req.user.id,
+          });
+          console.log("Post has been added!");
+          res.redirect("/profile");
+        } catch (err) {
+          console.log(err);
+        }
+      },
+}
